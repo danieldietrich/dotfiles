103659d2957c9d7a9420cdbe1a6439bf4518d733
Daniel Dietrich <cafebab3@gmail.com>
2022-10-15 00:41:10 +0200

    naming

--- scripts/zsh_addons_install.sh
+++ scripts/zsh_addons_install.sh
@@ -3,7 +3,7 @@
 # (C)opyright 2022 by Daniel Dietrich - MIT license
 # ------------------------------------------------------------------------------
 
-__install_zsh_addon() {
+__install() {
   local addon="$1"
   local init_script="$2"
   echo "Installing $addon..." >&2
@@ -13,14 +13,14 @@ __install_zsh_addon() {
 
 printf '\n\n%s\n' "export ZSH_DISABLE_COMPFIX=\"true\"" >> ~/.profile
 
-__install_zsh_addon zsh-autosuggestions "$(cat <<- CODE
+__install zsh-autosuggestions "$(cat <<- CODE
 if [ -f "\$HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" ]; then
   . \$HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh &>/dev/null
 fi
 CODE
 )"
 
-__install_zsh_addon zsh-history-substring-search "$(cat <<- CODE
+__install zsh-history-substring-search "$(cat <<- CODE
 if [ -f "\$HOME/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh" ]; then
   . \$HOME/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh &>/dev/null
   bindkey '^[[A' history-substring-search-up
@@ -30,7 +30,7 @@ fi
 CODE
 )"
 
-__install_zsh_addon zsh-completions "$(cat <<- CODE
+__install zsh-completions "$(cat <<- CODE
 if [ -d "\$HOME/.zsh/zsh-completions" ]; then
   FPATH="\$HOME/.zsh/zsh-completions":\$FPATH
   autoload -Uz compinit
