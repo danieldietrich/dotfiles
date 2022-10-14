f69c3090ea41472e860354b567dad4ce14673d0f
Daniel Dietrich <cafebab3@gmail.com>
2022-10-15 00:43:16 +0200

    aliases

---
+++ scripts/aliases.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ALIASES="$(cat <<- CODE
+# Git
+alias branch="git branch -vv"
+alias commit="git commit -m"
+alias fetch="git fetch --all --prune"
+alias pull="git pull --rebase"
+alias push="git push"
+alias rebase="git rebase -i"
+alias status="git status -sb"
+CODE
+)"
+
+for script in ".bashrc" ".zshrc"; do
+    printf '\n\n%s\n' "$ALIASES" >> "$HOME/$script"
+done
