97d6f178625b0e564e90d5a2c9d4560e072aa50c
Daniel Dietrich <cafebab3@gmail.com>
2022-10-16 04:01:12 +0200

    Add git alias checkout

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -3,6 +3,7 @@
 ALIASES="$(cat <<- CODE
 # Git
 alias branch="git branch -vv"
+alias checkout="git checkout"
 alias commit="git commit -m"
 alias fetch="git fetch --all --prune"
 alias pull="git pull --rebase"
